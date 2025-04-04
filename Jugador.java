a60e529c6d109d0c7eb441e59192a80f01492b51
Gerard <gerardjalon@gmail.com>
2025-04-05 00:45:31 +0200

    Modificar Jugador con Carta

--- Jugador.java
+++ Jugador.java
@@ -1,13 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Jugador {
     private String id;
     private String nombre;
     private double saldo;
+    private List<Carta> mano; 
     
     public Jugador(String id, String nombre, double saldo) {
         this.id = id;
         this.nombre = nombre;
         this.saldo = saldo;
+        this.mano = new ArrayList<Carta>(); 
+
     }
+    
+    public void recibirCarta(Carta carta) {
+        mano.add(carta);
+    }
+
+    public int calcularPuntos() {
+        int total = 0;
+        int ases = 0;
+        for (Carta carta : mano) {
+            total += carta.getValorNumerico();
+            if (carta.getValorNumerico() == 11) ases++;
+        }
+
+        while (total > 21 && ases > 0) {
+            total -= 10;
+            ases--;
+        }
+        return total;
+    }
+
+    
+    public void mostrarMano() {
+        for (Carta carta : mano) {
+            System.out.println(carta);
+        }
+        System.out.println("Puntos: " + calcularPuntos());
+    }
+
 
     public String getId() {
         return id;
@@ -28,5 +62,5 @@ public class Jugador {
         this.saldo = saldo;
     }
 
-    
+
 }
