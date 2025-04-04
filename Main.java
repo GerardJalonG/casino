0da7c3c2b9000a59d4291508fc83ff0d680dd144
Gerard <gerardjalon@gmail.com>
2025-04-05 00:26:50 +0200

    Crear Main, Java en VS no es del todo bien

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenido al Casino!");
+        System.out.println("Elija un juego para comenzar:");
+        System.out.println("1. Póker");
     }
 }
