2b8934037d72a68c1dfd93e3c0d160f194a5c23a
Gerard <gerardjalon@gmail.com>
2025-04-05 00:32:00 +0200

    Crear Clase Carta

--- Carta.java
+++ Carta.java
@@ -1,3 +1,28 @@
 public class Carta {
+    private String palo;
+    private String valor;
 
+    public Carta(String palo, String valor) {
+        this.palo = palo;
+        this.valor = valor;
+    }
+
+    public int getValorNumerico(){
+        if(valor.equals("A")){
+            return 11;
+        }else if(valor.equals("K")|| valor.equals("Q") || valor.equals("J")){
+            return 10;
+        }else{
+            return Integer.parseInt(valor);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return valor + " de " + palo;
+    }
+
+    
+
+    
 }
